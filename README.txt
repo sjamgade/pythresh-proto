4f47b3c8a9c6dd7248b54b569414037e6ae74bfa
Sumit Jamgade <sjamgade@suse.com>
2019-08-20 13:28:56 +0200

    diff to make dynamic tables possible

---
+++ README.txt
@@ -0,0 +1,3 @@
+tabletry branch has a trial for for tables being being created dynamically.
+however it needs a patch to faust as there seems to be a bug around that behaviour
+of adding tables dynamically.
--- thresh.py
+++ thresh.py
@@ -2,22 +2,48 @@
 import json
 import faust
 import pymysql
+import asyncio
 from collections import defaultdict
-from datetime import timedelta
+from datetime import timedelta, datetime
+
+from faust import Sensor
 
 ALARM_SQL = "select * from alarm_definition where deleted_at is NULL order by created_at"
 SUB_ALARM_SQL = """select sad.*, sadd.* from sub_alarm_definition sad 
     left outer join sub_alarm_definition_dimension sadd on sadd.sub_alarm_definition_id=sad.id 
     where sad.alarm_definition_id = '%s' order by sad.id"""
 
+
 app = faust.App('prototyping', broker='kafka://localhost:9092')
+
+# this sensor can be used for waiting to begin processing again 
+# and not skip at all
+class RebalanceSensor(Sensor):
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.rebstart = asyncio.Event()
+        self.rebend = asyncio.Event()
+        self.waiting_tables = dict()
+
+    def on_rebalance_start(self, *args, **kwargs):
+        self.rebstart.set()
+        self.rebstart.clear()
+
+    def on_rebalance_end(self, state):
+        pass
+
+
+
+#rebalance_sensor = RebalanceSensor()
+#app.sensors.add(rebalance_sensor)
 
 metric_topic = app.topic('metrics')
 
 
 tables = dict()
-alarms = []
 mapto = defaultdict(list)
+table_creation_time = dict()
+
 
 @app.agent(metric_topic)
 async def print_metrics(stream):
@@ -31,51 +57,60 @@ async def print_metrics(stream):
             for dim in dimensions.items():
                 al = mapto.get((metric_name, dim), None)
                 if al:
-                    key = (metric_name, dim)
-                    measure = f'{metric_name}-{dim[0]}-{dim[1]}'
-                    await handle_table_creation.send(key=measure, value=event.message.value)
-                    await handle_alarm_metrics.send(value=event.message.value)
+                    key = f'{metric_name}-{dim[0]}-{dim[1]}'.encode()
+                    value = metric
+                    if key not in tables.keys():
+#                        rebalance_sensor.rebstart.clear()
+#                        rebalance_sensor.state = ( key, app.Table(key.decode(), default=list, partitions=64).hopping(60, 5, expires=timedelta(minutes=10)) )
+                        table_creation_time[key] = datetime.now()
+                        tables[key] = app.Table(key.decode(), default=list, partitions=64).hopping(60, 5, expires=timedelta(minutes=10))
+                        app.consumer._thread.request_rejoin()
+                        #await rebalance_sensor.rebstart.wait()
+                        #await rebalance_sensor.rebend.wait()
+                        print(f'{key} rebalance end')
+                        #await app.topics.wait_for_subscriptions()
+                    if datetime.now() > (table_creation_time[key] + timedelta(seconds=5)):
+                        tables[key][value['name']] += [value['value']]
+                    else:
+                        print(f'skipping {key}')
         except Exception as e:
             print('*'*80)
-            print(e)
+            print(str(e))
             print('*'*80)
 
-@app.agent()
-async def handle_table_creation(measurements):
-    async for key,value in measurements.items():
-        if key not in tables.keys():
-
-            tables[key] = app.Table(key, default=list)
-            print(f'{tbname} created succsessfully')
-            
+#@app.agent()
+#async def handle_table_creation(measurements):
+#    async for key,value in measurements.items():
 
 
-
-@app.agent()
-async def handle_alarm_metrics(metrics):
-    async for msg in metrics:
-        print(msg)
+#@app.agent()
+#async def handle_alarm_metrics(metrics):
+#    async for metric in metrics:
+#        pass
 
 @app.agent(app.topic('events'))
 async def handle_alarm_definitions(stream):
     async for msg in stream:
         if 'alarm-definition-created' in msg.keys():
-            pass
+            print(msg.values)
         elif 'alarm-definition-deleted' in msg.keys():
-            pass
+            print(msg.values)
         elif 'alarm-definition-updated' in msg.keys():
-            pass
+            print(msg.values)
 
+#@app.timer(2.0)
+#async def dumpinfo():
+#    for key in tables:
+#        print(tables[key]['cpu.percent'].delta(10))
 
 @app.task
 async def create_infra():
-    global alarms, mapto
+    global mapto
     connection = pymysql.connect('127.0.0.1', 'root', 'secretdatabase', 'mon')
     cursor = connection.cursor(pymysql.cursors.DictCursor)
     cursor.execute(ALARM_SQL)
-    alarms += cursor.fetchall()
 
-    for al in alarms:
+    for al in cursor.fetchall():
         al['seen'] = False
         cursor.execute(SUB_ALARM_SQL % al['id'])
         al['subexpression'] = cursor.fetchall()
